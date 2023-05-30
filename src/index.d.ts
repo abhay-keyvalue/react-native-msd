8943e3b6686a9011dd6ce5841e5e1e16ecb5fdc5
Abhay <abhay@keyvalue.systems>
2023-05-30 11:43:40 +0530

    fix: update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,34 @@
+import type { IGetRecommendationRequest } from './recommendations/types.ts';
+
 declare module 'index' {
   export const useEvents: () => {
     track: (eventName: string, params: object) => Promise<void>;
   };
 
   export const useRecommendations: () => {
-    getRecommendations: (params: object) => Promise<void>;
+    getRecommendationByStrategy: (
+      strategy_reference: string,
+      properties: IGetRecommendationRequest
+    ) => Promise<void>;
+    getRecommendationByModule: (
+      module_reference: string,
+      properties: IGetRecommendationRequest
+    ) => Promise<void>;
+    getRecommendationByPage: (
+      page_reference: string,
+      properties: IGetRecommendationRequest
+    ) => Promise<void>;
+    getRecommendationByText: (
+      text_reference: string,
+      properties: IGetRecommendationRequest
+    ) => Promise<void>;
     recommendations: object;
   };
 
-  export const init: (key: string) => Promise<void>;
+  export const init: (options: {
+    token: string;
+    baseUrl: string;
+  }) => Promise<void>;
+
+  export const setUser: (options: { userId: string }) => Promise<void>;
 }
